6e9434e72094e2e069af96673d9f03ca99291059
Mayte B <20260202-ieg@instituto.ad>
2026-08-11 10:11:40 -0300

    feat: adiciona o Model.java padrão do DAO

---
+++ src/main/java/br/com/zera/model/Model.java
@@ -0,0 +1,24 @@
+package br.com.zera.model;
+
+/**
+ * Interface padrão com atributos de todas as entidades model
+ * 
+ * Define um contrato mínimo para que as classes de modelo forneçam
+ * um identificador único por meio do método {@link #getCodigo()}.
+ * 
+ * Essa interface é utilizada pela camada genérica de persistência
+ * ("DAO<T extends Model>"), permitindo que o DAO execute
+ * operações de forma padronizada sobre qualquer entidade,
+ * independentemente da sua estrutura interna.
+ * 
+ * @author Mayte B
+ */
+
+public interface Model {
+    /**
+     * Retorna o identificador único da entidade.
+     * @return objeto que representa o ID da entidade
+     */
+
+    Object getCodigo()
+}
