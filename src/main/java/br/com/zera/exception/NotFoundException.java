279782b2a681f4ad40216b329e363e4f8dce7b47
PedroRufino21 <seu-email-da-conta-pessoal@email.com>
2026-08-11 18:23:22 -0300

    feature: Adiciona a classe NotFoundException

---
+++ src/main/java/br/com/zera/exception/NotFoundException.java
@@ -0,0 +1,26 @@
+package br.com.zera.exception;
+
+/**
+ * Exceção lançada quando uma busca por ID ou atributo não encontra nenhum registro no banco de dados.
+ */
+public class NotFoundException extends ZeraException {
+
+    /**
+     * Constrói a exceção com uma mensagem direta.
+     *
+     * @param message Mensagem descrevendo o item não encontrado.
+     */
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constrói a mensagem de erro formatada informando a entidade e o código buscado.
+     *
+     * @param entidade Nome da tabela ou entidade (ex: "Endereco", "Unidade").
+     * @param id Código identificador que não foi localizado.
+     */
+    public NotFoundException(String entidade, int id) {
+        super(entidade + " com o código " + id + " não foi encontrado(a) no sistema.");
+    }
+}
