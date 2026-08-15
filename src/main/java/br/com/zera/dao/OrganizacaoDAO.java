9ffee60088a5a7ef56c009cbbf673afd2d758b58
PedroRufino21 <seu-email-da-conta-pessoal@email.com>
2026-08-15 11:32:06 -0300

    fix: Arruma um import

--- src/main/java/br/com/zera/dao/OrganizacaoDAO.java
+++ src/main/java/br/com/zera/dao/OrganizacaoDAO.java
@@ -3,6 +3,7 @@ package br.com.zera.dao;
 import br.com.zera.exception.ConnectionFailedException;
 import br.com.zera.exception.NotFoundException;
 import br.com.zera.model.Organizacao;
+import br.com.zera.util.Conexao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
