2b230c9af2ccaec7d3250f615d263d1a6a89d137
Mayte B <20260202-ieg@instituto.ad>
2026-09-09 08:22:04 -0300

    fix: Adiciona link correto para página de erro Erro.html

--- src/main/java/br/com/zera/regex/Constants.java
+++ src/main/java/br/com/zera/regex/Constants.java
@@ -30,5 +30,5 @@ public class Constants {
     public static final String CNPJ_REGEX = "^[0-9A-Z]{2}\\.?[0-9A-Z]{3}\\.?[0-9A-Z]{3}\\/?[0-9A-Z]{4}\\-?[0-9]{2}$";
 
     // Tela de erro
-    public static final String ERROR_PAGE = "^/WEB-INF/Erro.html";
+    public static final String ERROR_PAGE = "src/main/webapp/WEB-INF/Erro.html";
 }
